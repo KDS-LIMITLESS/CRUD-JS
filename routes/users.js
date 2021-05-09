fc9300360c9ac38a54a0477b8f3bc0f0a16d32b7
KDS-Limitless <limitlesskds@gmail.com>
2021-05-09 17:27:50 +0100

    CRUD functions

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,45 @@
 const mongoose = require('mongoose');
 const express = require('express');
+
 const router = express.Router();
 
+// User Model
+
 const User = mongoose.model('User', new mongoose.Schema({
-    name: String,
+    name: {type: String, required: true},
     country: String,
-    email: String
+    email: {type: String, unique: true}
 }));
 
-router.post('/', (req, res) => {
-    const user = new User({
+router.get('/', async (req, res) => {
+    let user = await User.find().sort('email')
+    res.send(user);
+});
+
+router.post('/', async (req, res) => { 
+    let newUser = new User({
         name: req.body.name,
         country: req.body.country,
         email: req.body.email
     });
-    await user.save();
+    user = await newUser.save();
     res.send(user);
+    console.log(typeof(newUser))
 });
+
+router.put('/:email', async (req, res) => {
+    User.findOne({email: req.params.email}, function(err, user){
+        if (err) return res.status(404).send(`${err}`);
+        if (user){
+            user['name'] = req.body.name,
+            user['email'] = req.body.email,
+            user['country'] = req.body.country
+            user.save()
+            res.send(user)
+        }else{
+            if (!user) res.status(404).send(`Not Found!`);
+        };
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const users = require('./routes/users')
 
 const app = express()
 
-const URL = ``
+const URL = `mongodb+srv://user1:kAuaV2zr0pm30QsV@cluster0.0l8d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const connetionParams = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 }
 
 mongoose.connect(URL, connetionParams)
-    .then(() => console.log("Connected to database")) 
+    .then(() => console.log("Connected to database"))
     .catch(err => console.error(`Connection to db failed. ${err}`))
 
+app.use(express.json())
+app.use('/api/users', users)
 
-app.listen(3000, () => {
-    console.log(`Server up and running..`)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server up and running on port ${port}..`)
 });
